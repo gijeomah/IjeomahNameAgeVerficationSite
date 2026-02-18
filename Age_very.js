36984876f4c492fda7a3b81b1831b41548542cc6
Genesis Ijeomah <gijeomah@my.waketech.edu>
2026-02-18 13:49:39 -0500

    Done

--- Age_very.js
+++ Age_very.js
@@ -1,16 +1,30 @@
 //creating 3 varables using the const keyword
 const myText = document.getElementById("ageInput");
 const mySubmit = document.getElementById("submitBtn");
-const myResult = document.getElementById("resultText");
+const myResult = document.getElementById("textbox");
+const myTest = document.getElementById("resultTxt")
 let age;
 mySubmit.onclick = function(){
-    age = myText.ariaValueMax;
+    console.log("   ");
+    age = myText.value;
     age = Number(age);
-    if (age < 5){
-        myResult.textContent = "Your less than 5 years old!";
+    if (age <= 5){
+        myTest.textContent = "You are less than 5 years old!";
     }
-    else if ( age < 13){
-        myResult.textContent
+    else if ( age < 18){
+        myTest.textContent = "You are starting school now!";
+    }
+    else if (age == 18){
+        myTest.textContent = "You are an  adult now ";
+    }
+      else if (age > 65){
+        myTest.textContent = "You are a Senior Citizens";
+    }
+    else if (age >= 21){
+        myTest.textContent = "You can drink!";
+    }
+    else{
+       myTest.textContent = "Invalid input";
     }
 
-};
+}
